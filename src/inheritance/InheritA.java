2f7319a42380e85c880e18c6f1de17e7f2ceba06
sathdurg <sathdurg1407@gmail.com>
2020-05-15 22:27:48 +0530

    Basic Inheritance project

---
+++ src/inheritance/InheritA.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class InheritA extends InheritB {
+	public static void main(String[] args) {
+		InheritA obj = new InheritA();
+		System.out.println("ChildClass");
+		obj.does();
+	}
+}
---
+++ src/inheritance/InheritB.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class InheritB {
+	// System.out.println("Teaching");
+	String Topic = "Inheritance";
+	String Language = "Java";
+
+	void does() {
+		System.out.println("ParentClass-Teaching");
+	}
+}
