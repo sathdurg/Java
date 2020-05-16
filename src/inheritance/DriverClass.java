78cd92f02cb79d516fd3fc02ae68b5bdae7fcc75
sathdurg <sathdurg1407@gmail.com>
2020-05-16 12:31:33 +0530

    Updated the program

---
+++ src/inheritance/DriverClass.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class DriverClass {
+
+	public static void main(String args[]) {
+
+		MountainBike mb = new MountainBike(3, 100, 25);
+		System.out.println(mb.toString());
+
+	}
+}
--- src/inheritance/InheritA.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class InheritA extends InheritB {
-	public static void main(String[] args) {
-		InheritA obj = new InheritA();
-		System.out.println("ChildClass");
-		obj.does();
-	}
-}
