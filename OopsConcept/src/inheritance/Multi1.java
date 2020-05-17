e9ff54f360cb8d71c80c25c483bcde40cb5fe02d
sathdurg <sathdurg1407@gmail.com>
2020-05-17 20:38:36 +0530

    Added the project

---
+++ OopsConcept/src/inheritance/Multi1.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+//Cyclic Inheritance not permitted
+//public class Multi1 extends Multi2 {
+public class Multi1 {
+	public void show() {
+		System.out.println("Sathish");
+	}
+}
---
+++ OopsConcept/src/inheritance/SubClass2.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class SubClass2 extends SubClass{
+	public String java()
+	{
+		System.out.println(super.sample());
+		return ("for Java");
+		
+	}
+
+    
+}
